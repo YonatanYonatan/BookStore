12d836f50ed971ed0523724f784125fcc09b38f0
Oron Ben Zvi <oronbz@gmail.com>
2015-07-08 17:09:45 +0300

    coderev

--- app.js
+++ app.js
@@ -1,60 +1,173 @@
+/**
+ * Created by Yonatan.Vainer on 6/23/2015.
+ */
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var busboy =  require('connect-busboy');
+var fs = require('fs-extra');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var books =
+    [
+        
+        {
+            "title": "boo",
+            "author": "kremer",
+            "year": "1995",
+            "id": 1,
+            "link": "",
+            "cover": "images/Folder.jpg"
+        },
+        {
+            "title": "foo",
+            "author": "george",
+            "year": "1991",
+            "id": 2,
+            "link": "",
+            "cover": "images/Folder.jpg"
+        },
+        {
+            "title": "doo",
+            "author": "koko",
+            "year": "1995",
+            "id": 3,
+            "link": "",
+            "cover": "images/Folder.jpg"
+        },
+        {
+            "title": "hoo",
+            "author": "jerry",
+            "year": "1980",
+            "id": 4,
+            "link": "",
+            "cover": "images/Folder.jpg"
+        }
+        
+    ];
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.use(bodyParser());
+app.use(busboy({immediate: true}));
 
-app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+/**
+ * Oron: put all routes on different file, see example: https://github.com/oronbz/mean-starter-kit
+ */
 
-// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+/**
+ * Oron: save to an external folder (like 'upload') and add to .gitignore
+ */
+app.post('/images', function(req, res, next){
+
+    var fstream;
+
+    req.busboy.on('file', function(fieldname, file, filename){
+
+        fstream = fs.createWriteStream('./public/images/'+filename);
+        file.pipe(fstream);
+        fstream.on('close', function(){
+            console.log('upload finished for '+filename);
+        });
+
     });
-  });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    req.busboy.on('finish', function(){
+        next();
+    });
+
+    }, function(req, res){
+
+    res.sendStatus(200);
+
 });
 
 
-module.exports = app;
+app.get('/books', function(req, res){
+    res.send(books);
+});
+ 
+/**
+ * Oron: add function per HTTP method (DELETE, PUT(edit))
+ * also add id to urls when possible
+ */
+app.post('/', function (req, res) {
+
+    var opp = req.body.opp;
+
+    if (opp == "insert"){
+        var title = req.body.data.title;
+        var author = req.body.data.author;
+        var year = req.body.data.year;
+        var link = req.body.data.link;
+        var cover = "";
+
+        if (req.body.data.cover != ""){
+            cover = "http://localhost:3000/images/"+req.body.data.cover
+        }
+
+        var book = {title: title, author: author, year: year, link: link, cover: cover};
+
+        var max_id = -1;
+
+        for (var i=0; i<books.length; i++){
+            if (books[i].id > max_id){
+                max_id = books[i].id;
+            }
+        }
+
+        book.id = max_id + 1;
+
+        books.push(book);
+    }
+
+    else if (opp == "delete"){
+        var id = req.body.data.id;
+
+        for (var i=0; i<books.length; i++){
+            if (books[i].id == id){
+                books.splice(i,1);
+                break;
+            }
+        }
+    }
+
+    else if (opp == "edit"){
+
+        var id = req.body.data.id;
+
+        for (var i=0; i<books.length; i++){
+            if (books[i].id == id){
+
+                if (req.body.data.title != ""){
+                    books[i].title = req.body.data.title;
+                }
+                if (req.body.data.year != ""){
+                    books[i].year = req.body.data.year;
+                }
+                if (req.body.data.author != ""){
+                    books[i].author = req.body.data.author;
+                }
+                if (req.body.data.link != ""){
+                    books[i].link = req.body.data.link;
+                }
+
+
+                break;
+            }
+        }
+    }
+
+    res.sendStatus(200);
+
+});
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('app listening at http://%s:%s', host, port);
+
+});
